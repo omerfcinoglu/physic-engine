c531d8e5b2f2363ba7af297019b934cb15fa603e
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-05-03 18:12:58 +0300

    Update Particle.cs
    add collision reflect and basic move

--- physic-engine/Assets/Scripts/Particle.cs
+++ physic-engine/Assets/Scripts/Particle.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
+    public float radius = 1f;
     SpriteRenderer spriteRenderer;
+    public Vector2 velocity;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.color = Random.ColorHSV();
     }
 
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        velocity += Physics2D.gravity * Time.deltaTime;
+        transform.position += (Vector3)(velocity * Time.deltaTime);
+    }
+
+    public void HandleCollision(Particle other)
+    {
+        Vector2 collisionNormal = (transform.position - other.transform.position).normalized;
+        velocity = Vector2.Reflect(velocity, collisionNormal);
+    }
 }
